884fcf238d39944f3a49a4444b4cb0b88c1887a3
Dmitrii Semikin <dmitry.semikin@gmail.com>
2021-03-09 21:25:28 +0100

    Minor formatting.

--- include/Shiny.h
+++ include/Shiny.h
@@ -7,14 +7,16 @@
 extern "C" {
 #endif
 
-// ShinyCommons.h must be included into all sources and headers as a first include
+
+/* ShinyCommons.h must be included into all sources and headers as a first include. */
 #include "ShinyCommons.h"
 
 #include "ShinyMacros.h"
 #include "ShinyManager.h"
 
+
 #ifdef __cplusplus
-}
+} // extern "C"
 #endif
 
 #endif /* SHINY_H */
