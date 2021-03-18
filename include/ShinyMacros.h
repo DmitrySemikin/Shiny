321389e0fc6f16cffdc5a6495dfcb70e501d4237
Dmitrii Semikin <dmitry.semikin@gmail.com>
2021-03-18 20:33:42 +0100

    looking at macros

--- include/ShinyMacros.h
+++ include/ShinyMacros.h
@@ -236,23 +236,29 @@ SHINY_INLINE ShinyData GetEmptyData() {
 }
 
 #define PROFILE_UPDATE()
+#define PROFILE_CLEAR()
+#define PROFILE_DESTROY()
+
 #define PROFILE_SET_DAMPING(x)
 #define PROFILE_GET_DAMPING()			0.0f
+
 #define PROFILE_OUTPUT(x)
 #define PROFILE_OUTPUT_STREAM(x)
-#define PROFILE_CLEAR()
 #define PROFILE_GET_TREE_STRING()		std::string()
 #define PROFILE_GET_FLAT_STRING()		std::string()
-#define PROFILE_DESTROY()
+
 #define PROFILE_BEGIN(name)
 #define PROFILE_BLOCK(name)
+
 #define PROFILE_FUNC()
 #define PROFILE_CODE(code)				do { code; } while (0)
+
 #define PROFILE_SHARED_GLOBAL(name)
 #define PROFILE_SHARED_MEMBER(name)
 #define PROFILE_SHARED_DEFINE(name)
 #define PROFILE_SHARED_BEGIN(name)
 #define PROFILE_SHARED_BLOCK(name)
+
 #define PROFILE_GET_SHARED_DATA(name)	ShinyGetEmptyData()
 #define PROFILE_GET_ROOT_DATA()			ShinyGetEmptyData()
 
